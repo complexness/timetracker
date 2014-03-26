93cd18f9b3c7684946cb6016af0924ddf86ee048
Lex Javier <lex.javier@gmail.com>
2014-03-27 01:52:18 +0800

    Revert "Generate AngularFire support in the AngularJS app, add ObserveJS manually to app/index.html because of generator-angularfire error"
    This reverts commit 00a745f6fb5e03e8c469817e14cbd735d2b96911.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,22 +4,13 @@ angular.module('timetrackerApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute',
-  'firebase',
-  'angularfire.firebase',
-  'angularfire.login',
-  'simpleLoginTools'
+  'ngRoute'
 ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/login', {
-        authRequired: false, // if true, must log in before viewing this page
-        templateUrl: 'views/login.html',
-        controller: 'LoginController'
       })
       .otherwise({
         redirectTo: '/'
